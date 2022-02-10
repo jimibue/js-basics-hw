a02a9ce1936f3872e602930c753ae5b6a0cb1539
James <jimibue@gmail.com>
2022-02-10 11:52:16 -0800

    reads

---
+++ homework.js
@@ -0,0 +1,46 @@
+
+
+// WARM UPS
+
+
+//0. DESCRIBE IN YOUR OWN WORDS THE DIFFERENCE BETWEEN LET AND CONST
+
+
+//1. Write a for loop that logs only logs even numbers from 0 - 10; ie 0,2,4,..10
+     
+
+//2. Write a for that logs numbers from 10 - 0; ie 10,9,8...0
+
+
+//3. write a for loop that loops through this array and logs 'odd/even' based on wether the number is odd even
+let nums = [1,2,3,4,5,6,7,8,9,10]
+
+//4. write a while loop that starts at 0 and counts up to 10 ie: 0,1,2,3....10
+
+
+//5. Look closely at the code below
+const isOdd = (num) =>{
+    return num % 2 === 0
+}
+
+if(isOdd(2)){
+    console.log('is Odd')
+} else {
+    console.log('is Even')
+}
+
+//5a. what will this log?
+
+
+//5b. what is the value of isOdd(2) 
+
+
+//5c. Fix the isOdd function to make this work
+
+
+
+
+
+
+
+
